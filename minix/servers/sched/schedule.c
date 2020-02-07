de0879f607258ac66d4855fbd3bbc22f7517e0f5
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-02-07 12:19:01 +0530

    aaaaa

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -87,9 +87,11 @@ static void pick_cpu(struct schedproc * proc)
 
 long long timeInMilliseconds(void) {
     struct timeval tv;
-
+	printf("1");
     gettimeofday(&tv,NULL);
-    return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+	printf("2");
+    // return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+	return 200
 }
 
 /*===========================================================================*
