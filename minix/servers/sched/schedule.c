b1149173715b9f39936792c7a25b6b80d0fda2db
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-02-09 19:29:35 +0530

    Change Priority Order

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -100,7 +100,7 @@ int do_noquantum(message *m_ptr)
 
 	rmp = &schedproc[proc_nr_n];
 	if (rmp->priority < MIN_USER_Q) {
-		rmp->priority += 1; /* lower priority */
+		rmp->priority -= 1; /* increase priority */
 	}
 
 	if ((rv = schedule_process_local(rmp)) != OK) {
@@ -324,7 +324,9 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 		printf("PM: An error occurred when trying to schedule %d: %d\n",
 		rmp->endpoint, err);
 	}
-
+	if(rmp->priority >= 7 && rmp->max_priority == 7) {
+		printf("PID: %d swapped in\n", _ENDPOINT_P(rmp->endpoint));
+	}
 	return err;
 }
 
@@ -357,7 +359,7 @@ static void balance_queues(minix_timer_t *tp)
 	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
 		if (rmp->flags & IN_USE) {
 			if (rmp->priority > rmp->max_priority) {
-				rmp->priority -= 1; /* increase priority */
+				// rmp->priority -= 1; /* increase priority */
 				schedule_process_local(rmp);
 			}
 		}
