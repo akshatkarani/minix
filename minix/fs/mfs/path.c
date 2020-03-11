4ab1eed73684d7a2b90abda4a531f35d18d2e807
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-03-11 17:55:05 +0530

    dd dele

--- minix/fs/mfs/path.c
+++ minix/fs/mfs/path.c
@@ -557,6 +557,7 @@ int check_permissions;		 /* check permissions when flag is !IS_EMPTY */
 			r = OK;
 			if (flag == IS_EMPTY) r = ENOTEMPTY;
 			else if (flag == DELETE) {
+				printf("Deleted");
 				/* Save d_ino for recovery. */
 				t = MFS_NAME_MAX - sizeof(ino_t);
 				*((ino_t *) &dp->mfs_d_name[t]) = dp->mfs_d_ino;
