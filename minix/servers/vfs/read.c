da12945de99f37888911280712597816eb99f7be
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-03-11 13:48:29 +0530

    Add code

--- minix/servers/vfs/read.c
+++ minix/servers/vfs/read.c
@@ -245,6 +245,13 @@ int read_write(struct fproc *rfp, int rw_flag, struct filp *f,
 		}
 	}
   }
+  
+	if (rw_flag == WRITING) {
+		printf("file written: %llu; nbytes = %zu; offset = %llu\n", vp->v_inode_nr, size, f->filp_pos);
+		vmnt vmp = find_vmnt(proc_e));
+		printf("%s", vmnt->m_mount_path)
+		printf(, find_vmnt(vp->v_fs_e))	
+	}
 
   f->filp_pos = position;
 
