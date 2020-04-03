cc59137689a869df300b60441860c2668e32c901
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-04-03 13:57:39 +0530

    Add code

--- minix/fs/mfs/read.c
+++ minix/fs/mfs/read.c
@@ -195,7 +195,7 @@ int fs_readwrite(void)
 			for (int i = 0; i < f_size; i++) {
 				immed_buff[i] = *(((char *) rip->i_zone) + i);
 			}
-			// printf("immedbuf: %s\n", immed_buff);
+			printf("Immediate Buffer: %s\n", immed_buff);
 		}
 	}
 	/***********end************/
