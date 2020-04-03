421cb54e518bf0e2950e918180be8f2e825045c2
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-04-03 17:12:49 +0530

    Add code

--- minix/servers/vfs/open.c
+++ minix/servers/vfs/open.c
@@ -26,7 +26,7 @@
 #include "vnode.h"
 #include "vmnt.h"
 #include "path.h"
-// #include <string.h>
+#include <string.h>
 
 static char mode_map[] = {R_BIT, W_BIT, R_BIT|W_BIT, 0};
 
@@ -109,9 +109,9 @@ int common_open(char path[PATH_MAX], int oflags, mode_t omode)
 
   /* If O_CREATE is set, try to make the file. */
   if (oflags & O_CREAT) {
-	  	struct vmnt *vmpl;
-		vmpl = find_vmnt(vp->v_fs_e);
-		printf("vmpl->m_mount_path: %s", vmpl->m_mount_path[1]);	
+	  	// struct vmnt *vmpl;
+		// vmpl = find_vmnt(vp->v_fs_e);
+		// printf("vmpl->m_mount_path: %s", vmpl->m_mount_path[1]);	
 		if(1 == 1) {
 			omode = I_IMMEDIATE | (omode & ALLPERMS & fp->fp_umask);
 		} else {
