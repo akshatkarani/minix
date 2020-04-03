af6ac86e827a0e0dce1b8dadf413e4a062d8db14
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-04-03 17:20:40 +0530

    Add code

--- minix/servers/vfs/open.c
+++ minix/servers/vfs/open.c
@@ -109,14 +109,7 @@ int common_open(char path[PATH_MAX], int oflags, mode_t omode)
 
   /* If O_CREATE is set, try to make the file. */
   if (oflags & O_CREAT) {
-	  	// struct vmnt *vmpl;
-		// vmpl = find_vmnt(vp->v_fs_e);
-		// printf("vmpl->m_mount_path: %s", vmpl->m_mount_path[1]);	
-		if(1 == 1) {
-			omode = I_IMMEDIATE | (omode & ALLPERMS & fp->fp_umask);
-		} else {
-			omode = I_REGULAR | (omode & ALLPERMS & fp->fp_umask);
-		}
+		omode = I_IMMEDIATE | (omode & ALLPERMS & fp->fp_umask);
 	vp = new_node(&resolve, oflags, omode);
 	r = err_code;
 	if (r == OK) exist = FALSE;	/* We just created the file */
