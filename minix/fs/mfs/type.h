d9943c1a633bd1d11cdf4f8e6ec6fd1e359dfe7d
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-04-03 12:02:18 +0530

    add code

--- minix/fs/mfs/type.h
+++ minix/fs/mfs/type.h
@@ -17,20 +17,20 @@ typedef struct {		/* V2.x disk inode */
 } d2_inode;
 
 
-// struct buf {
-//   /* Data portion of the buffer. */
-//   union fsdata_u *bp;
+struct buf {
+  /* Data portion of the buffer. */
+  union fsdata_u *bp;
 
-//   /* Header portion of the buffer. */
-//   struct buf *b_next;           /* used to link all free bufs in a chain */
-//   struct buf *b_prev;           /* used to link all free bufs the other way */
-//   struct buf *b_hash;           /* used to link bufs on hash chains */
-//   block_t b_blocknr;            /* block number of its (minor) device */
-//   dev_t b_dev;                  /* major | minor device where block resides */
-//   char b_dirt;                  /* CLEAN or DIRTY */
-//   char b_count;                 /* number of users of this buffer */
-//   unsigned int b_bytes;         /* Number of bytes allocated in bp */
-// };
+  /* Header portion of the buffer. */
+  struct buf *b_next;           /* used to link all free bufs in a chain */
+  struct buf *b_prev;           /* used to link all free bufs the other way */
+  struct buf *b_hash;           /* used to link bufs on hash chains */
+  block_t b_blocknr;            /* block number of its (minor) device */
+  dev_t b_dev;                  /* major | minor device where block resides */
+  char b_dirt;                  /* CLEAN or DIRTY */
+  char b_count;                 /* number of users of this buffer */
+  unsigned int b_bytes;         /* Number of bytes allocated in bp */
+};
 
 #endif
 
