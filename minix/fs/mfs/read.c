eae28ce18d1693adcfdd13224c2f63d8535aebd1
agrawalarpit14 <agrawal.arpit14@gmail.com>
2020-04-06 20:40:06 +0530

    Add code

--- minix/fs/mfs/read.c
+++ minix/fs/mfs/read.c
@@ -164,7 +164,7 @@ int fs_readwrite(void)
       for (int i = 0; i < f_size; i++) {
 				immed_buff[i] = *(((char *) rip->i_zone) + i);
 			}
-			printf("%s\n", immed_buff);
+			printf("%s", immed_buff);
     }
 	}
 
